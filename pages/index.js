fd89be769013a9d9055c2e817de66428cad7cf7b
joshacross <joshuaalexandercross@gmail.com>
2022-03-13 00:20:44 -0600

    added mission cards, about, and connect section to home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import PageLayout from "../layouts/PageLayout";
 import styles from "../styles/Home.module.css";
 import HeroArt from "../assets/hero-art.png";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, Grid, Container, Card, CardContent, CardActions, FormGroup, FormControl, FormHelperText, InputLabel, Input, TextField } from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <PageLayout>
+    <PageLayout sx={{ background: "#eaeaea" }}>
       <Box display="flex" sx={{ background: "#D9D0B8" }}>
         <Box
           display="flex"
@@ -32,10 +32,150 @@ export default function Home() {
             Get Started
           </Button>
         </Box>
-
         <Box flex={2}>
           <Image src={HeroArt} alt="Hero art" />
         </Box>
+        <Box
+          display="flex"
+          flex={1}
+          alignContent="center"
+          flexDirection="column"
+          justifyContent="center"
+          pr={2}
+          pl={2}
+          pt={3}
+          pb={3}
+        ></Box>
+      </Box>
+
+      {/* Three Box Mission */}
+      <Box sx={{ py: 6 }} maxWidth="full">
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card
+              display="flex"
+              flex={1}
+              alignContent="center"
+              flexDirection="column"
+              justifyContent="center"
+              sx={{ height: "100%", background: "#E3B4B4" }}
+            >
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Build
+                </Typography>
+                <Typography>
+                  This is a media card. You can use this section to describe the
+                  content.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More...</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card
+              display="flex"
+              flex={1}
+              alignContent="center"
+              flexDirection="column"
+              justifyContent="center"
+              sx={{ height: "100%", background: "#98CAD2" }}
+            >
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Get Funded
+                </Typography>
+                <Typography>
+                  This is a media card. You can use this section to describe the
+                  content.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More...</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card
+              display="flex"
+              flex={1}
+              alignContent="center"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              sx={{ height: "100%", background: "#E37965" }}
+            >
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Grow
+                </Typography>
+                <Typography>
+                  This is a media card. You can use this section to describe the
+                  content.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More...</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+      {/* ABOUT */}
+      <Box display="flex">
+        <Box
+          display="flex"
+          flex={1}
+          alignContent="center"
+          flexDirection="column"
+          justifyContent="center"
+          pr={4}
+          pl={3}
+          pt={2}
+          pb={3}
+          sx={{ py: 6, background: "#" }}
+        >
+          <Typography variant="h4">About</Typography>
+          <Typography>
+            This is a paragraph Ipsum labore ut tempor anim ut veniam sint
+            reprehenderit. Cillum do proident laboris eiusmod exercitation id eu
+            id laborum. Occaecat cillum sint est exercitation ad cillum. Aliqua
+            exercitation nisi laborum occaecat irure tempor laboris dolor anim
+            ea. Incididunt nisi aliquip reprehenderit enim voluptate enim labore
+            enim. Aliqua cupidatat labore enim officia laborum elit adipisicing
+            deserunt dolor cillum pariatur mollit ex sunt.
+          </Typography>
+        </Box>
+        <Box
+          display="flex"
+          flex={1}
+          alignContent="center"
+          flexDirection="column"
+          justifyContent="center"
+          pr={4}
+          pl={3}
+          pt={2}
+          pb={3}
+          sx={{ py: 6, background: "#98CAD2" }}
+        >
+          Image
+        </Box>
+      </Box>
+      {/* CONTACT */}
+      <Box display="flex" flexDirection="column">
+        <Typography variant="h4">
+          Contact
+        </Typography>
+        <FormControl>
+        <Box display="flex" alignContent="center" justifyContent="space-between">
+          <TextField id="standard-basic" label="Name" variant="standard" required />
+          <TextField id="standard-basic" label="Email" variant="standard" required helperText="required"/>
+        </Box>
+          <TextField multiline minRows="" id="outline-basic" label="Message" variant="outlined" />
+          <Button>Submit</Button>
+        </FormControl>
       </Box>
     </PageLayout>
   );
